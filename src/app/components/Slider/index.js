ae8646745b19f5b6e4971a027138b17c298ebaec
MaikonWeber <maikonweber@gmail.com>
2024-01-15 13:26:42 -0300

    Add Content

---
+++ src/app/components/Slider/index.js
@@ -0,0 +1,11 @@
+const Content = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="max-w-md p-6 text-center">
+                <h1 className="text-2xl font-bold mb-4">Turning ideas into extraordinary interactive experiences, where gameplay is the essence that captivates and challenges</h1>
+                <p className="text-gray-600">Comming Soon.</p>
+            </div>
+        </div>)
+}
+
+export default Content
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import Content from './components/Slider'
 export default function Home() {
   return (
     <main>
       <Header />
+      <Content />
       <Footer />
     </main>
   )
