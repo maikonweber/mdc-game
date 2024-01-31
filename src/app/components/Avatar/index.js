d0474ffa4d0e9f3956ec47782b2f2e7e35146514
MaikonWeber <maikonweber@gmail.com>
2024-01-31 17:36:46 -0300

    Add Perfil de Icon

--- src/app/components/Avatar/index.js
+++ src/app/components/Avatar/index.js
@@ -8,31 +8,19 @@ const AvatarCompoent = () => {
 ]
 
     return (
-        <div class="flex bg-white sm:flex-row p-2 w-fit bg-gradient-to-r from-white to-blue-200 rounded-full shadow-md">
+        <div class="flex bg-white p-2 w-fit bg-gradient-to-r from-white to-blue-200 rounded-full shadow-md">
         <div>
-        <Image class="rounded-full ml-5 w-20 h-20" width={100} height={100} src="https://avatars.githubusercontent.com/maikonweber" alt="Avatar Image"/> 
-        <h2 class="text-sm text-center ml-2 font-semibold"> Develop </h2>
-        </div>
+        <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/maikonweber" alt="Avatar Image"/>         </div>
        <div>
-        <Image class="rounded-full ml-5 w-20 h-20" width={100} height={100} src="https://avatars.githubusercontent.com/chegos" alt="Avatar Image"/> 
-        <h2 class="text-sm text-center ml-2 font-semibold"> Develop </h2>
-        </div>
+        <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/chegos" alt="Avatar Image"/>         </div>
         <div>
-        <Image class="rounded-full ml-5 w-20 h-20" width={100} height={100} src="https://avatars.githubusercontent.com/fbatisteti" alt="Avatar Image"/> 
-        <h2 class="text-sm text-center ml-2 font-semibold"> Develop </h2>
-        </div>
+        <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/fbatisteti" alt="Avatar Image"/>         </div>
         <div>
-        <Image class="rounded-full ml-5 w-20 h-20" width={100} height={100} src="https://avatars.githubusercontent.com/PedroSchene" alt="Avatar Image"/> 
-        <h2 class="text-sm text-center ml-2 font-semibold"> Develop </h2>
-        </div>
+        <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/PedroSchene" alt="Avatar Image"/>         </div>
         <div>
-        <Image class="rounded-full ml-5 w-20 h-20" width={100} height={100} src="https://avatars.githubusercontent.com/paulodinizlima" alt="Avatar Image"/> 
-        <h2 class="text-sm text-center ml-2 font-semibold"> Develop </h2>
-        </div>
+        <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/paulodinizlima" alt="Avatar Image"/>         </div>
         <div>
-        <Image class="rounded-full ml-5 w-20 h-20" width={100} height={100} src="https://avatars.githubusercontent.com/FogacaDev" alt="Avatar Image"/> 
-        <h2 class="text-sm text-center ml-2 font-semibold"> Develop </h2>
-        </div>
+        <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/FogacaDev" alt="Avatar Image"/>         </div>
         
         
     </div>
