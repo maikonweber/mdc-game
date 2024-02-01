4fe39c1e270811eb357d3fcb0d7b60b6639da8df
MaikonWeber <maikonweber@gmail.com>
2024-02-01 13:38:35 -0300

    Add new image of coorporation

--- src/app/components/Avatar/index.js
+++ src/app/components/Avatar/index.js
@@ -11,7 +11,7 @@ const AvatarCompoent = () => {
         <div class="flex bg-white p-2 w-fit bg-gradient-to-r from-white to-blue-200 rounded-full shadow-md">
         <div>
         <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/maikonweber" alt="Avatar Image"/>         </div>
-       <div>
+        <div>
         <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/chegos" alt="Avatar Image"/>         </div>
         <div>
         <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/fbatisteti" alt="Avatar Image"/>         </div>
@@ -21,6 +21,8 @@ const AvatarCompoent = () => {
         <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/paulodinizlima" alt="Avatar Image"/>         </div>
         <div>
         <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/FogacaDev" alt="Avatar Image"/>         </div>
+        <div>
+        <Image class="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/rlucascc" alt="Avatar Image"/>         </div>
         
         
     </div>
