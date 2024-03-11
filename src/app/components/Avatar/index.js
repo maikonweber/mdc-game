29de93f7411df735db6c91598215b96b0136d924
Maikon Weber <maikonweber@gmail.com>
2024-03-10 23:05:20 -0300

    Add Team Button

--- src/app/components/Avatar/index.js
+++ src/app/components/Avatar/index.js
@@ -1,24 +1,22 @@
 import Image from "next/image"
+import Link from 'next/link';
 
 const AvatarCompoent = () => {
     const arrayAvatar = [{
-        url : "https://avatars.githubusercontent.com/maikonweber",
+        url: "https://avatars.githubusercontent.com/maikonweber",
         class: "Develop"
     }
-]
+    ]
+
+
 
     return (
-        <div className="flex bg-white p-2 w-fit bg-gradient-to-r from-white to-blue-200 rounded-full shadow-md">
-        <Image className="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/maikonweber" alt="Avatar Image"/>         
-        <Image className="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/chegos" alt="Avatar Image"/>         
-        <Image className="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/fbatisteti" alt="Avatar Image"/>         
-        <Image className="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/PedroSchene" alt="Avatar Image"/>         
-        <Image className="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/paulodinizlima" alt="Avatar Image"/>         
-        <Image className="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/FogacaDev" alt="Avatar Image"/>         
-        <Image className="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/rlucascc" alt="Avatar Image"/>         
-        <Image className="rounded-full ml-5 w-7 h-7 md:w-20 md:h-20" width={50} height={50} src="https://avatars.githubusercontent.com/lucaspetean" alt="Avatar Image"/>         
-    </div>
-    )
+        <Link href="/new-page">
+            <div className="flex bg-white p-2 w-fit bg-gradient-to-r from-white to-blue-200 rounded-full shadow-md" >
+                <h1> TEAM </h1>
+            </div>
+        </Link>
+    );
 }
 
 export default AvatarCompoent
